e6e9e93ac0281eb5d300770cef2d3a14a8bd23b6
EerieEight <80974420+EerieEight@users.noreply.github.com>
2022-02-02 22:50:43 -0500

    deployed

--- scripts/deploy_brewboo.ts
+++ scripts/deploy_brewboo.ts
@@ -1,19 +1,19 @@
 import { ethers, run } from "hardhat"
 
-boo = "0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE"
-wftm = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83"
-factory = "0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3"
-xboo = "0xa48d959AE2E88f1dAA7D5F611E01908106dE7598"
+const boo = "0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE"
+const wftm = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83"
+const factory = "0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3"
+const xboo = "0xa48d959AE2E88f1dAA7D5F611E01908106dE7598"
 
 async function main() {
     const brewBoo = await ethers.getContractFactory("BrewBoo");
-    BrewBoo = await brewBoo.deploy(factory, BooMirrorWorld.address, boo, wftm);
+    const BrewBoo = await brewBoo.deploy(factory, xboo, boo, wftm);
     await BrewBoo.deployed()
     console.log("BrewBoo deployed to:", BrewBoo.address);
 
     await run("verify:verify", {
       address: BrewBoo.address,
-      constructorArguments: [factory, BooMirrorWorld.address, boo, wftm],
+      constructorArguments: [factory, xboo, boo, wftm],
   })
 }
 
