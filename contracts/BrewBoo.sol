f1f283949a749a3dbd7c6c6fac75b6a96f2b17b9
EerieEight <80974420+EerieEight@users.noreply.github.com>
2022-02-06 21:05:10 -0500

    merge to 1 require

--- contracts/BrewBoo.sol
+++ contracts/BrewBoo.sol
@@ -161,8 +161,7 @@ contract BrewBoo is Ownable, ReentrancyGuard {
                 require(!isLpToken(token0[i]), "no LP allowed");
                 continue;
             }
-            require(!isLpToken(token0[i]), "no LP allowed");
-            require(!isLpToken(token1[i]), "no LP allowed");
+            require(!isLpToken(token0[i]) && !isLpToken(token1[i]), "no LP allowed");
             IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(token0[i], token1[i]));
             require(address(pair) != address(0), "BrewBoo: Invalid pair");
 
